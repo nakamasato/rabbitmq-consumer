f6fa34ab7564fe7647c1811df9b0fbf918198852
Naka Masato <masatonaka1989@gmail.com>
2021-03-21 08:51:21 +0900

    Init commit

---
+++ src/main/java/com/naka/rabbitmqconsumer/App.java
@@ -0,0 +1,13 @@
+package com.naka.rabbitmqconsumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
