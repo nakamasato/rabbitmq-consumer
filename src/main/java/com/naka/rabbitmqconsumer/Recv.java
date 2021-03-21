5eab19e59c11e9314295b436249f57995e8e8082
Naka Masato <masatonaka1989@gmail.com>
2021-03-21 10:19:41 +0900

    Enable to configure PROCESS_SECONDS to delay processing each message (#2)

--- src/main/java/com/naka/rabbitmqconsumer/Recv.java
+++ src/main/java/com/naka/rabbitmqconsumer/Recv.java
@@ -4,6 +4,7 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
+import java.util.concurrent.TimeUnit;
 
 public class Recv {
     private final static String QUEUE_NAME = "hello";
@@ -15,13 +16,20 @@ public class Recv {
         factory.setPassword(System.getenv().getOrDefault("RABBITMQ_PASSWORD", "guest"));
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
+        int processSeconds = Integer.parseInt(System.getenv().getOrDefault("PROCESS_SECONDS", "10"));
 
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        System.out.println("Waiting for messages. To exit press CTRL+C");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
-            System.out.println(" [x] Received '" + message + "'");
+            System.out.println("Received '" + message + "'. It will take " + processSeconds + " seconds to complete processing.");
+            try {
+                TimeUnit.SECONDS.sleep(processSeconds);
+            } catch(InterruptedException e) {
+                System.out.println("sleep failed");
+            }
+            System.out.println("Finished Processing '" + message + "'");
         };
         channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
         });
