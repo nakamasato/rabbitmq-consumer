e7cc3527db0c1e8e5fef9c583555900970aa41e8
Naka Masato <masatonaka1989@gmail.com>
2021-03-26 10:27:23 +0900

    add date (#4)

--- src/main/java/com/naka/rabbitmqconsumer/Recv.java
+++ src/main/java/com/naka/rabbitmqconsumer/Recv.java
@@ -5,6 +5,8 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class Recv {
     private final static String QUEUE_NAME = "hello";
@@ -20,16 +22,19 @@ public class Recv {
 
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         System.out.println("Waiting for messages. To exit press CTRL+C");
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
-            System.out.println("Received '" + message + "'. It will take " + processSeconds + " seconds to complete processing.");
+            Date date = new Date(System.currentTimeMillis());
+            System.out.println(formatter.format(date) + " Received '" + message + "'. It will take " + processSeconds + " seconds to complete processing.");
             try {
                 TimeUnit.SECONDS.sleep(processSeconds);
             } catch(InterruptedException e) {
                 System.out.println("sleep failed");
             }
-            System.out.println("Finished Processing '" + message + "'");
+            date = new Date(System.currentTimeMillis());
+            System.out.println(formatter.format(date) + " Finished Processing '" + message + "'");
         };
         channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
         });
